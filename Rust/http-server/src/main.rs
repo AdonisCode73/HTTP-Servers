2332c4148661cc9a2cfadd725372a54ff9f25b9f
Adam <adoniscode73@gmail.com>
2026-06-11 01:18:07 +0100

    RUST HTTP parsing implemented

--- Rust/http-server/src/main.rs
+++ Rust/http-server/src/main.rs
@@ -1,35 +1,71 @@
-use std::{io::Read, net::{Shutdown, TcpListener, TcpStream}};
+use std::{io::{BufRead, BufReader, Read}, net::{Shutdown, TcpListener, TcpStream}};
 use anyhow::{Context, Ok, Result};
 use std::collections::HashMap;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct HTTPRequest {
     method: String,
     path:   String,
     version:    String,
     headers:    HashMap<String, String>,
-    body:       u8
+    body:      Vec<u8> 
 }
 
 fn parse_http_request(stream: &mut TcpStream) -> Result<HTTPRequest> {
-    let mut req = HTTPRequest::default();
+    let mut reader = BufReader::new(stream);
+   
+    let mut line = String::new();
+
+    reader.read_line(&mut line)?;
+
+    let mut parsed_header_iter = line.split_whitespace();
+
+    let method = parsed_header_iter.next().context("Request line missing method")?.to_string();
+    let path = parsed_header_iter.next().context("Request line missing path")?.to_string();
+    let version = parsed_header_iter.next().context("Request line missing version")?.to_string();
+
+    let mut headers = HashMap::new();
+    let mut body = vec![];
 
 
-    let mut buf = vec![];
-    let num_bytes_read = stream.read_to_end(&mut buf)?;
+    loop {
+        line.clear();
+        reader.read_line(&mut line)?;
 
-    //TODO: Turn the buf into a string OR manually read till whitespace, then get the index of
-    //whitespace and get a slice of the vec, removing the slice and appending to req.
-    Ok(req)
+        if line.eq("\r\n") {
+            break;
+        }
+
+        let split_header = line.trim().split_once(":").context("Header missing context")?;
+        headers.insert(split_header.0.trim().to_string(), split_header.1.trim().to_string());
+    }
+
+    if let Some(length) = headers.get("Content-Length") {
+        let mut buf = Vec::with_capacity(length.parse().context("Content length is malformed")?);
+        reader.read_exact(&mut buf)?;
+
+        body = std::mem::take(&mut buf);
+    }
+    
+    Ok(HTTPRequest {
+        method,
+        path,
+        version,
+        headers,
+        body,
+    })
+
+
 }
 
 fn handle_client(stream: &mut TcpStream) -> Result<()> {
     println!("Client successfully connected!");
 
-    parse_http_request(stream)?;
-    
+    let req =    parse_http_request(stream)?;
+    dbg!(&req); 
+    //println!("{:#?}", req);
 
-    //stream.shutdown(Shutdown::Both)?;
+    stream.shutdown(Shutdown::Both)?;
     Ok(())
 }
 fn main() -> Result<()>{
